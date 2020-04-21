bad02299b1ef8fcd7e2f77232304f659d9d8c8c7
hguerrav <63132256+hguerrav@users.noreply.github.com>
2020-04-20 22:42:46 -0600

    Add Team Controller

---
+++ Soccer.Web/Data/Enteties/TeamEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Data.Enteties
+{
+    public class TeamEntity
+    {
+        public int Id { get; set; }
+        [MaxLength(50, ErrorMessage = "The  field {0} can not have more that {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+
+        public string LogoPath { get; set; }
+    }
+}
